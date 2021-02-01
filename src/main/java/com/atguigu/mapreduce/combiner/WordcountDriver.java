398b0ef30ded38993e961544baec88b2b57f9cf0
lidongmin <376214287@qq.com>
2021-02-01 17:06:06 +0800

    新增mapredure压缩

--- src/main/java/com/atguigu/mapreduce/combiner/WordcountDriver.java
+++ src/main/java/com/atguigu/mapreduce/combiner/WordcountDriver.java
@@ -4,6 +4,8 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.compress.BZip2Codec;
+import org.apache.hadoop.io.compress.CompressionCodec;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -24,6 +26,13 @@ public class WordcountDriver {
 
         // 1 获取配置信息以及封装任务
         Configuration configuration = new Configuration();
+
+        // 开启map端输出压缩
+        configuration.setBoolean("mapreduce.map.output.compress", true);
+        // 设置map端输出压缩方式
+        configuration.setClass("mapreduce.map.output.compress.codec", BZip2Codec.class, CompressionCodec.class);
+
+
         Job job = Job.getInstance(configuration);
 
         // 2 设置jar加载路径
@@ -40,6 +49,11 @@ public class WordcountDriver {
         // 5 设置Reduce输出
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
+
+        // 设置reduce端输出压缩开启
+        FileOutputFormat.setCompressOutput(job, true);
+        // 设置压缩的方式
+        FileOutputFormat.setOutputCompressorClass(job, BZip2Codec.class);
 
         // 6 设置输入和输出路径
         FileInputFormat.setInputPaths(job, new Path(args[0]));
---
+++ src/test/java/TestCompress.java
@@ -0,0 +1,83 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.io.compress.CompressionCodecFactory;
+import org.apache.hadoop.io.compress.CompressionInputStream;
+import org.apache.hadoop.io.compress.CompressionOutputStream;
+import org.apache.hadoop.util.ReflectionUtils;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * @program: damein
+ * @description:
+ * @author: lidongmin
+ * @create: 2021-02-01 16:35
+ */
+public class TestCompress {
+
+    public static void main(String[] args) throws Exception {
+
+        String methodName = "org.apache.hadoop.io.compress.BZip2Codec";
+        compress("e:/hello.txt", methodName);
+        String decompressPath = "e:/hello.txt.bz2";
+        decompress(decompressPath);
+
+    }
+
+    private static void compress(String fileName, String method) throws Exception {
+
+        // （1）获取输入流
+        FileInputStream fis = new FileInputStream(fileName);
+
+        Class codecClass = Class.forName(method);
+
+        CompressionCodec codec = (CompressionCodec) ReflectionUtils.newInstance(codecClass, new Configuration());
+
+        // （2）获取输出流
+        FileOutputStream fos = new FileOutputStream(fileName + codec.getDefaultExtension());
+        CompressionOutputStream cos = codec.createOutputStream(fos);
+
+        // （3）流的对拷
+        IOUtils.copyBytes(fis, cos, 1024 * 1024 * 5, false);
+
+        // （4）关闭资源
+        fis.close();
+        cos.close();
+        fos.close();
+
+
+    }
+
+    // 2、解压缩
+    private static void decompress(String fileName) throws FileNotFoundException, IOException {
+
+        // （0）校验是否能解压缩
+        CompressionCodecFactory factory = new CompressionCodecFactory(new Configuration());
+
+        CompressionCodec codec = factory.getCodec(new Path(fileName));
+
+        if (codec == null) {
+            System.out.println("cannot find codec for file " + fileName);
+            return;
+        }
+
+        // （1）获取输入流
+        CompressionInputStream cis = codec.createInputStream(new FileInputStream(fileName));
+
+        // （2）获取输出流
+        FileOutputStream fos = new FileOutputStream(fileName + ".decoded");
+
+        // （3）流的对拷
+        IOUtils.copyBytes(cis, fos, 1024 * 1024 * 5, false);
+
+        // （4）关闭资源
+        cis.close();
+        fos.close();
+    }
+}
+}
